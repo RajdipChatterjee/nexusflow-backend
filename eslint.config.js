19c2c9cbb32853df6ce59cf9617de45daf117a26
Rajdip Chatterjee <rajdipchatterjee3000@gmail.com>
2026-05-29 02:24:26 +0530

    Re initializtion of  prettier, typescript, eslint.

--- eslint.config.js
+++ eslint.config.js
@@ -1,16 +1,14 @@
-import js from '@eslint/js';
-import globals from 'globals';
-import tseslint from 'typescript-eslint';
-import { defineConfig } from 'eslint/config';
-import eslintConfigPrettier from "eslint-config-prettier";
+import js from '@eslint/js'
+import globals from 'globals'
+import tseslint from 'typescript-eslint'
+import { defineConfig } from 'eslint/config'
+
 export default defineConfig([
     {
         files: ['**/*.{js,mjs,cjs,ts,mts,cts}'],
         plugins: { js },
         extends: ['js/recommended'],
-        languageOptions: { globals: { ...globals.browser, ...globals.node } },
+        languageOptions: { globals: globals.node },
     },
-    ...tseslint.configs.recommended,
-    eslintConfigPrettier,
-]);
-//# sourceMappingURL=eslint.config.js.map
+    tseslint.configs.recommended,
+])
